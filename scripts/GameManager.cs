c0a51259a7e919170e8d5cc71b3312521b8bd6a7
Ágúst Bergmann Sigurðsson <43780363+AugustBergmannS@users.noreply.github.com>
2020-04-05 00:25:25 +0000

    Add files via upload

---
+++ scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameManager : MonoBehaviour
+    {
+
+        public float restartDelay = 0.4f;
+
+        bool gameHasEnded = false;
+
+        public void EndGame()
+        {
+            if (gameHasEnded == false)
+            {
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+            }    
+        }
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
---
+++ scripts/movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private float timer = 2f;
+
+    public GameObject rb;
+
+    public float runSpeed = 20f;
+    float moveh = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+
+        
+
+
+    void Update()
+    {
+        moveh = Input.GetAxisRaw("Horizontal") * runSpeed;
+        timer += Time.deltaTime;
+        if (timer >= 2f)
+        { 
+            if(Input.GetButtonDown("Jump"))
+            {
+                jump = true;
+                Debug.Log("bbnopp");
+            }
+        }
+        /*else if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }*/
+        if(rb.transform.position.y < -1.4f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+    void FixedUpdate()
+    {
+        controller.Move(moveh * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
